3a7c9aed34f2f502efb92a56bdea812c0f3cfabe
Kekray <kekray777@gmail.com>
2022-05-13 19:21:53 +0300

    Created events

--- post-app/src/components/post-list-item/post-list-item.js
+++ post-app/src/components/post-list-item/post-list-item.js
@@ -25,7 +25,7 @@ export default class PostListItem extends Component{
     }
 
     render() {
-        const {label} = this.props;
+        const {label, onDelete} = this.props;
         const {important, like} = this.state;
         let classNames = 'app-list-item d-flex justify-content-between'
 
@@ -47,14 +47,15 @@ export default class PostListItem extends Component{
             </span>
             <div className="d-flex justify-content-center align-items-center">
                 <button 
-                type="button" 
+                type="button"  
                 className="btn-star btn-sm"
                 onClick={this.onImportant}>
                     <i className="fa fa-star"></i>
                 </button>
                 <button 
                 type="button" 
-                className="btn-trash btn-sm">
+                className="btn-trash btn-sm"
+                onClick={onDelete}>
                     <i className="fa fa-trash"></i>
                 </button>
                 <i className="fa fa-heart"></i>
